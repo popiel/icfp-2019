9a0199ced04a600111467260ec162c4c0c92fdd2
T. Alexander Popiel <tapopiel@gmail.com>
2019-06-21 10:36:57 -0500

    Adding in initial problem set and submission script

---
+++ submit.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd `dirname $0`
+[ -r solutions.zip ] || touch solutions.zip --date="10 days ago"
+find solutions -name '*.sol' -newer solutions.zip -print -quit | grep -qs '.' && zip -j -r solutions solutions -i '*.sol'
+[ -r next-submission ] || touch next-submission --date="10 days ago"
+[ solutions.zip -nt next-submission ] && curl -F "private_id=$(tail -1 token)" -F "file=@solutions.zip" https://monadic-lab.org/submit && touch next-submission --date="+10 minutes"
